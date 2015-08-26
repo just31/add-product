957966286202ef19c1db96cedb9c4831b60f35b1
Evgeny Moiseev <just8@mail.ru>
2015-08-26 17:56:17 +0300

    файл с настройками к базе данных

---
+++ config.php
@@ -0,0 +1,11 @@
+﻿<?php
+
+$username = "admin";
+$password = "admin";
+$hostname = "localhost";
+$databasename = "product";
+
+$connecDB = mysql_connect($hostname, $username, $password) or die('could not connect to database');
+mysql_select_db($databasename,$connecDB);
+
+?>
